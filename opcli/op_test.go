8f35ec86859af32681b168704fa3b2bfb146e6e7
tctgdanny <154454046+tctgdanny@users.noreply.github.com>
2024-06-29 18:16:44 -0500

    Add initial "VaultCreate" function and "VaultCreateOptions" struct
    TODO: Add solution for optional bools for "AllowAdminsToManage" option in struct

--- opcli/op_test.go
+++ opcli/op_test.go
@@ -18,6 +18,22 @@ func TestGetVaults(t *testing.T) {
 	}
 }
 
+func TestGetVaultsWithOptions(t *testing.T) {
+	client := Client{}
+	vaults, err := client.VaultList(VaultListOption{User: "Danny"})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(vaults) < 1 {
+		t.Log("No vaults available to list")
+	} else {
+		for _, vault := range vaults {
+			t.Logf("Vault: %s", vault.Name)
+		}
+	}
+}
+
 func TestGetVault(t *testing.T) {
 	client := Client{}
 	vault, err := client.VaultGet("kfoesdlf")
@@ -28,5 +44,17 @@ func TestGetVault(t *testing.T) {
 	// Output vault if not empty struct
 	if vault != (Vault{}) {
 		t.Logf("Vault: %s", vault.Name)
+	}
+}
+
+func TestCreateVault(t *testing.T) {
+	client := Client{}
+	vault, err := client.VaultCreate("Test Vault 2", VaultCreateOption{Description: "This is a vault"})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if vault != (Vault{}) {
+		t.Logf("Vault ID: %s", vault.ID)
 	}
 }
--- opcli/vault.go
+++ opcli/vault.go
@@ -12,6 +12,22 @@ type VaultListItem struct {
 	ContentVersion int64  `json:"content_version"`
 }
 
+// Options for filtering the list of available vaults
+type VaultListOption struct {
+	Group      string
+	Permission string
+	User       string
+}
+
+// Options for creation of a new vault.
+// If AllowAdminsToManage is not provided, the default policy for the account applies.
+type VaultCreateOption struct {
+	// TODO: Find clean solution for optional bools
+	// AllowAdminsToManage *bool
+	Description string
+	Icon        string
+}
+
 // Full details of a 1Password vault, output of "vault get" op command
 type Vault struct {
 	ID               string    `json:"id"`
@@ -26,9 +42,22 @@ type Vault struct {
 
 // Retrieves a list of available vaults with short summaries including ID, name, and content version.
 // Mostly used in order to get ID and name to pass to GetVault function.
-func (c *Client) VaultList() ([]*VaultListItem, error) {
+func (c *Client) VaultList(options ...VaultListOption) ([]*VaultListItem, error) {
+	args := []string{"list"}
+	for _, option := range options {
+		if option.Group != "" {
+			args = append(args, "--group", option.Group)
+		}
+		if option.Permission != "" {
+			args = append(args, "--permissions", option.Permission)
+		}
+		if option.User != "" {
+			args = append(args, "--user", option.User)
+		}
+	}
+
 	var out []*VaultListItem
-	err := c.RunOpUnmarshal("vault", []string{"list"}, &out)
+	err := c.RunOpUnmarshal("vault", args, &out)
 	if err != nil {
 		return nil, fmt.Errorf("error getting vaults list: %s", err)
 	}
@@ -43,6 +72,33 @@ func (c *Client) VaultGet(identifier string) (Vault, error) {
 	err := c.RunOpUnmarshal("vault", []string{"get", identifier}, &out)
 	if err != nil {
 		return Vault{}, fmt.Errorf("error getting details for vault %s: %s", identifier, err)
+	}
+
+	return out, nil
+}
+
+func (c *Client) VaultCreate(vaultName string, options ...VaultCreateOption) (Vault, error) {
+	args := []string{"create", vaultName}
+	for _, option := range options {
+		// If AllowAdminsToManage is set to true or false, add "--allow-admins-to-manage" and "true" or "false" to args
+
+		// TODO: Find clean solution for optional bools
+		// if option.AllowAdminsToManage != nil {
+		// 	boolStr := strconv.FormatBool(*option.AllowAdminsToManage)
+		// 	args = append(args, "--allow-admins-to-manage", boolStr)
+		// }
+		if option.Description != "" {
+			args = append(args, "--description", option.Description)
+		}
+		if option.Icon != "" {
+			args = append(args, "--description", option.Icon)
+		}
+	}
+
+	var out Vault
+	err := c.RunOpUnmarshal("vault", args, &out)
+	if err != nil {
+		return Vault{}, fmt.Errorf("error creating vault %s: %s", vaultName, err)
 	}
 
 	return out, nil
